d92eae3a039a307705b8c0ff18629a6cd294443f
nshipochka <nshipochka@gmail.com>
2024-11-26 12:26:44 +0200

    Add Week-08

---
+++ 08-tree/implementations/BinaryTree.h
@@ -0,0 +1,194 @@
+#pragma once
+#include <iostream>
+#include <queue> 
+
+template <typename T>
+class BinaryTree {
+private:
+    struct node {
+        T data;
+        node* left;
+        node* right;
+
+        node(T data = T(), node* left = nullptr, node* right = nullptr)
+            : data(data), left(left), right(right) {};
+    };
+
+public:
+    BinaryTree();
+    BinaryTree(T data, const BinaryTree& left = BinaryTree(), const BinaryTree& right = BinaryTree());
+    BinaryTree(const BinaryTree& other);
+    BinaryTree& operator=(const BinaryTree& other);
+    ~BinaryTree();
+
+    bool empty() const;
+    bool find(const T& key) const;
+    void insert(const T& key);
+    bool remove(const T& key);
+    size_t get_size() const;
+    size_t get_height() const;
+
+    template <typename Function>
+    void map(const Function& f);
+
+private:
+    static void clear(node* root);
+    static node* copy(node* root);
+
+    static bool find(const node* root, const T& key);
+    static node* insert(node* root, const T& key);
+    static node* remove(node* root, const T& key, bool& removed);
+    static size_t height(const node* root);
+
+    template <typename Function>
+    static void map(const Function& f, node* root);
+
+private:
+    node* root;
+    size_t size;
+};
+
+template <typename T>
+BinaryTree<T>::BinaryTree()
+    : root(nullptr), size(0) {}
+
+template <typename T>
+BinaryTree<T>::BinaryTree(T data, const BinaryTree& left, const BinaryTree& right)
+    : root(new node(data, copy(left.root), copy(right.root))), size(1 + left.size + right.size) {}
+
+template <typename T>
+BinaryTree<T>::BinaryTree(const BinaryTree& other)
+    : root(copy(other.root)), size(other.size) {}
+
+template <typename T>
+BinaryTree<T>& BinaryTree<T>::operator=(const BinaryTree& other) {
+    if (this != &other) {
+        clear(root);
+        root = copy(other.root);
+        size = other.size;
+    }
+    return *this;
+}
+
+template <typename T>
+BinaryTree<T>::~BinaryTree() {
+    clear(root);
+}
+
+template <typename T>
+bool BinaryTree<T>::empty() const {
+    return root == nullptr;
+}
+
+template <typename T>
+bool BinaryTree<T>::find(const T& key) const {
+    return find(root, key);
+}
+
+template <typename T>
+void BinaryTree<T>::insert(const T& key) {
+    root = insert(root, key);
+    ++size;
+}
+
+template <typename T>
+bool BinaryTree<T>::remove(const T& key) {
+    bool removed = false;
+    root = remove(root, key, removed);
+    if (removed) --size;
+    return removed;
+}
+
+template <typename T>
+size_t BinaryTree<T>::get_size() const {
+    return size;
+}
+
+template <typename T>
+size_t BinaryTree<T>::get_height() const {
+    return height(root);
+}
+
+template <typename T>
+template <typename Function>
+void BinaryTree<T>::map(const Function& f) {
+    map(f, root);
+}
+
+// Private methods
+template <typename T>
+void BinaryTree<T>::clear(node* root) {
+    if (!root) return;
+    clear(root->left);
+    clear(root->right);
+    delete root;
+}
+
+template <typename T>
+typename BinaryTree<T>::node* BinaryTree<T>::copy(node* root) {
+    if (!root) return nullptr;
+    return new node(root->data, copy(root->left), copy(root->right));
+}
+
+template <typename T>
+bool BinaryTree<T>::find(const node* root, const T& key) {
+    if (!root) return false;
+    if (root->data == key) return true;
+    return find(root->left, key) || find(root->right, key);
+}
+
+template <typename T>
+typename BinaryTree<T>::node* BinaryTree<T>::insert(node* root, const T& key) {
+    if (!root) return new node(key);
+    if (key < root->data) root->left = insert(root->left, key);
+    else root->right = insert(root->right, key);
+    return root;
+}
+
+template <typename T>
+typename BinaryTree<T>::node* BinaryTree<T>::remove(node* root, const T& key, bool& removed) {
+    if (!root) return nullptr;
+
+    if (key < root->data) {
+        root->left = remove(root->left, key, removed);
+    }
+    else if (key > root->data) {
+        root->right = remove(root->right, key, removed);
+    }
+    else {
+        removed = true;
+        if (!root->left) {
+            node* right_child = root->right;
+            delete root;
+            return right_child;
+        }
+        else if (!root->right) {
+            node* left_child = root->left;
+            delete root;
+            return left_child;
+        }
+        else {
+            node* successor = root->right;
+            while (successor->left) successor = successor->left;
+            root->data = successor->data;
+            root->right = remove(root->right, successor->data, removed);
+        }
+    }
+    return root;
+}
+
+template <typename T>
+size_t BinaryTree<T>::height(const node* root) {
+    if (!root) return 0;
+    return 1 + std::max(height(root->left), height(root->right));
+}
+
+template <typename T>
+template <typename Function>
+void BinaryTree<T>::map(const Function& f, node* root) {
+    if (!root) return;
+    f(root->data);
+    map(f, root->left);
+    map(f, root->right);
+}
+
---
+++ 08-tree/implementations/GeneralTree.h
@@ -0,0 +1,236 @@
+﻿#pragma once
+
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class GeneralTree {
+private:
+	struct node {
+		T data;
+		node* child;
+		node* sibling;
+
+		node(T data = T(), node* child = nullptr, node* sibling = nullptr)
+			: data(data), child(child), sibling(sibling) {};
+	};
+
+public:
+	GeneralTree();
+	GeneralTree(T data, const GeneralTree& child = GeneralTree(), const GeneralTree& sibling = GeneralTree());
+	GeneralTree(const GeneralTree& other);
+	GeneralTree& operator=(const GeneralTree& other);
+	~GeneralTree();
+
+	bool empty() const;
+	bool find(const T& key) const;
+	bool remove(const T& key);
+	size_t get_size() const;
+	size_t get_height() const;
+
+	template <typename Function>
+	void map(const Function& f);
+
+	void print() const;
+	void print_by_levels() const;
+
+private:
+	static void clear(node* root);
+	static typename GeneralTree<T>::node* copy(node* root);
+
+	static bool find(const node* root, const T& key);
+	static size_t height(const node* root);
+	static bool remove(node*& root, const T& key);
+	static void print_by_levels(const node* root);
+
+	template <typename Function>
+	void map(const Function& f, node* root);
+
+private:
+	node* root;
+	size_t size;
+};
+
+template<typename T>
+inline GeneralTree<T>::GeneralTree()
+	: root(nullptr), size(0) {}
+
+template<typename T>
+inline GeneralTree<T>::GeneralTree(T data, const GeneralTree& child, const GeneralTree& sibling) {
+	root = new node(data, copy(child.root), copy(sibling.root));
+}
+
+template<typename T>
+inline GeneralTree<T>::GeneralTree(const GeneralTree<T>& other)
+	: root(copy(other.root))
+	, size(other.size) {}
+
+template<typename T>
+inline GeneralTree<T>& GeneralTree<T>::operator=(const GeneralTree<T>& other) {
+	if (this != &other) {
+		clear(root);
+		root = copy(other.root);
+		size = other.size;
+	}
+
+	return *this;
+}
+
+template<typename T>
+inline GeneralTree<T>::~GeneralTree() {
+	clear(root);
+}
+
+template<typename T>
+inline bool GeneralTree<T>::empty() const {
+	return root == nullptr;
+}
+
+template<typename T>
+inline bool GeneralTree<T>::find(const T& key) const {
+	return find(root, key);
+}
+
+template<typename T>
+inline bool GeneralTree<T>::remove(const T& key) {
+	return remove(root, key);
+}
+
+template<typename T>
+inline size_t GeneralTree<T>::get_size() const {
+	return size;
+}
+
+template<typename T>
+inline size_t GeneralTree<T>::get_height() const {
+	return height(root);
+}
+
+template<typename T>
+template<typename Function>
+inline void GeneralTree<T>::map(const Function& f) {
+	map(f, root);
+}
+
+template<typename T>
+inline void GeneralTree<T>::print() const {
+	map([](const T& x) { std::cout << x << ' '; });
+	std::cout << std::endl;
+}
+
+template<typename T>
+inline void GeneralTree<T>::print_by_levels() const {
+	print_by_levels(root);
+}
+
+// Private methods
+template<typename T>
+inline void GeneralTree<T>::clear(node* root) {
+	if (!root) return;
+
+	clear(root->child);
+	clear(root->sibling);
+	delete root;
+}
+
+template<typename T>
+inline typename GeneralTree<T>::node* GeneralTree<T>::copy(node* root) {
+	if (!root)
+		return root;
+
+	return new
+		node(root->data,
+			copy(root->child),
+			copy(root->sibling));
+}
+
+template<typename T>
+inline bool GeneralTree<T>::find(const node* root, const T& key) {
+	if (!root)
+		return false;
+
+	if (root->data == key)
+		return true;
+
+	return find(root->sibling, key)
+		|| find(root->child, key);
+}
+
+template<typename T>
+inline size_t GeneralTree<T>::height(const node* root) {
+	if (!root)
+		return 0;
+
+	return std::max(
+		1 + height(root->child),
+		height(root->sibling));
+}
+
+template<typename T>
+inline bool GeneralTree<T>::remove(node*& root, const T& key) {
+	if (!root)
+		return false;
+
+	if (root->data != key)
+		return remove(root->sibling, key)
+		|| remove(root->child, key);
+
+	node* to_delete = root;
+
+	if (!root->child) {
+		root = root->sibling;
+	}
+	else if (!root->sibling) {
+		root = root->child;
+	}
+	else {
+		// Взимаме всички наследници на root
+		// и ги закачаме към децата на съседа му
+		node* it = root->child;
+		while (it->sibling)
+			it = it->sibling;
+
+		it->sibling = root->sibling->child;
+		root->sibling->child = root->child;
+		root = root->sibling;
+	}
+
+	--size;
+	delete to_delete;
+	return true;
+}
+
+template<typename T>
+inline void GeneralTree<T>::print_by_levels(const node* root) {
+	if (!root)
+		return;
+
+	std::queue<const node*> q;
+	q.push(root);
+	q.push(nullptr); // За ново ниво
+	while (true) {
+		const node* curr = q.front();
+		q.pop();
+		if (!curr) {
+			std::cout << std::endl;
+			if (q.empty())
+				return;
+			q.push(nullptr);
+		}
+		else {
+			std::cout << curr->data << " ";
+			for (const node* it = curr->child; it; it = it->sibling)
+				q.push(it);
+		}
+	}
+}
+
+template<typename T>
+template<typename Function>
+inline void GeneralTree<T>::map(const Function& f, node* root) {
+	if (!root) return;
+
+	f(root->data);
+	map(f, root->sibling);
+	map(f, root->child);
+}
