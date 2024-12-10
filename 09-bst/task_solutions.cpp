1bae9feacb6bd0a3888b18e7b0b28c61799a4335
nshipochka <nshipochka@gmail.com>
2024-12-10 10:56:15 +0200

    add last task

--- 09-bst/task_solutions.cpp
+++ 09-bst/task_solutions.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <sstream>
 //#include "bst.h"
+#include <unordered_map>
 
+using std::unordered_map;
 using std::string;
 using std::stringstream;
 
@@ -77,6 +79,51 @@ int sum(const node* root) {
 		return 0;
 
 	return root->data + sum(root->left) + sum(root->right);
+}
+
+// Last task: solution O(n^2)
+int weight(const node* root) {
+	if (!root)
+		return 0;
+
+	return 1 + weight(root->left) + weight(root->right);
+}
+
+int count_n_children(const node* root, int n) {
+	if (!root)
+		return 0;
+
+	// If root has weight n, it's children must have weight < n
+	if (weight(root) == n)
+		return 1;
+	
+	return count_n_children(root->left, n)
+		+  count_n_children(root->right, n);
+}
+
+// Last task: hash segway
+int store_weight(const node* root, unordered_map<const node*, int>& weight) {
+	if (!root)
+		return 0;
+
+	int left_weight = store_weight(root->left, weight);
+	int right_weight = store_weight(root->right, weight);
+
+	weight[root] = 1 + left_weight + right_weight;
+
+	return weight[root];
+}
+
+int count_n_children(const node* root, int n, unordered_map<const node*, int>& weight) {
+	if (!root)
+		return 0;
+
+	int count = weight[root] == n ? 1 : 0;
+
+	count += count_n_children(root->left, n);
+	count += count_n_children(root->right, n);
+
+	return count;
 }
 
 int main() {
