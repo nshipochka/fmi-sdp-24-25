a418bd7713ce979c8a488e532d1ea7c96d98667c
nshipochka <nshipochka@gmail.com>
2024-12-19 13:11:01 +0200

    Solutions from class

---
+++ 11-exam-prep/Task1.cpp
@@ -0,0 +1,229 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <string>
+#include <sstream>
+
+using std::vector;
+using std::pair;
+using std::array;
+using std::string;
+using std::stringstream;
+
+enum Drinks {
+	NONE = -1,
+
+	BEER,
+	VODKA,
+	WHISKEY,
+	TEQUILA,
+	COLA,
+	RUM,
+	GIN,
+	MINT,
+
+	COUNT
+};
+
+const array<string, COUNT> drinks = 
+	{ "beer", "vodka", "whiskey", "tequila", "cola", "rum", "gin", "mint" };
+
+struct BarSlot {
+	vector<Drinks> available_drinks;
+	BarSlot* next;
+
+	bool is_available;
+
+	BarSlot(const vector<Drinks>& drinks = vector<Drinks>(), BarSlot* next = nullptr, bool is_available = true)
+		: available_drinks(drinks), next(next), is_available(is_available) {
+	}
+};
+
+struct Client {
+	vector<Drinks> favourite_drinks;
+	
+	Client* prev;
+	Client* next;
+
+	bool is_seated;
+
+	Client(const vector<Drinks>& drinks = vector<Drinks>(), Client* prev = nullptr, Client* next = nullptr, bool is_seated = false)
+		: favourite_drinks(drinks), prev(prev), next(next), is_seated(is_seated) {
+	}
+};
+
+
+BarSlot* create_bar(std::initializer_list<pair<vector<Drinks>, bool>> list) {
+	BarSlot dummy;
+	BarSlot* curr = &dummy;
+	for (auto element : list) {
+		curr->next = new BarSlot(element.first, nullptr, element.second);
+		curr = curr->next;
+	}
+
+	return dummy.next;
+}
+
+Client* create_group(std::initializer_list<pair<vector<Drinks>, bool>> list) {
+	Client dummy;
+	Client* curr = &dummy;
+	for (auto element : list) {
+		curr->next = new Client(element.first, curr, nullptr, element.second);
+		curr = curr->next;
+	}
+
+	dummy.next->prev = nullptr;
+	return dummy.next;
+}
+
+void clear(BarSlot*& head) {
+	BarSlot* curr = head;
+
+	while (curr) {
+		BarSlot* to_delete = curr;
+		curr = curr->next;
+		delete to_delete;
+	}
+
+	head = nullptr;
+}
+
+void clear(Client*& head) {
+	Client* curr = head;
+
+	while (curr) {
+		Client* to_delete = curr;
+		curr = curr->next;
+		delete to_delete;
+	}
+
+	head = nullptr;
+}
+
+bool drinks_available(const vector<Drinks>& bar, const vector<Drinks>& client) {
+	for (auto drink : client) {
+		if (find(bar.begin(), bar.end(), drink) == bar.end())
+			return false;
+	}
+
+	return true;
+}
+
+bool place(BarSlot* bar, Client* clients) {
+	// bar == nullptr, clients == nullptr;
+
+	int skipped_seats = 0;
+
+	while (bar && clients) {
+		if (bar->is_available && drinks_available(bar->available_drinks, clients->favourite_drinks)) {
+			bar->is_available = false;
+			clients->is_seated = true;
+			clients = clients->next;
+			skipped_seats = 0;
+		}
+		else {
+			skipped_seats++;
+		}
+
+		if (skipped_seats > 5)
+			return false;
+
+		bar = bar->next;
+	}
+
+	if (!clients)
+		return true;
+
+	return false;
+}
+
+template <typename T>
+void get_permutations(vector<vector<T>>& res, vector<T> arr, int index) {
+	if (index == arr.size()) {
+		res.push_back(arr);
+		return;
+	}
+
+	// Този индекс сложен на всяка възможна позиция до края
+	for (int i = index; i < arr.size(); i++) {
+		std::swap(arr[index], arr[i]);
+		get_permutations(res, arr, index + 1);
+
+		// Backtracking
+		std::swap(arr[index], arr[i]);
+	}
+}
+
+BarSlot* copy(const BarSlot* bar) {
+	BarSlot* bar_copy = nullptr;
+	while (bar) {
+		bar_copy = new BarSlot(bar->available_drinks, bar_copy, bar->is_available);
+		bar = bar->next;
+	}
+
+	return bar_copy;
+}
+
+bool place_permutation(BarSlot* bar, vector<Client*> groups) {
+	for (int i = 0; i < groups.size(); ++i) {
+		if (!place(bar, groups[i]))
+			return false;
+	}
+	return true;
+}
+
+bool placeAll(BarSlot* bar, vector<Client*> groups) {
+	vector<vector<Client*>> permutations;
+	get_permutations(permutations, groups, 0);
+
+	for (size_t i = 0; i < permutations.size(); ++i) {
+		BarSlot* bar_copy = copy(bar);
+		if (place_permutation(bar_copy, permutations[i])) {
+			clear(bar_copy);
+			return true;
+		}
+		clear(bar_copy);
+	}
+
+	return false;
+}
+
+bool place_all_no_perm() {
+	
+}
+
+int main() {
+	BarSlot* bar = create_bar({ 
+		{{VODKA, COLA, RUM}, true}, 
+		{{GIN, BEER, WHISKEY, COLA, MINT}, true},
+		{{BEER, VODKA, WHISKEY, TEQUILA, COLA, RUM, GIN, MINT}, true},
+		{{BEER, WHISKEY, TEQUILA, COLA, RUM, GIN, MINT}, true},
+		{{BEER, VODKA, WHISKEY, RUM, GIN, MINT}, true},
+		{{BEER, VODKA, WHISKEY}, true},
+		{{WHISKEY, TEQUILA, COLA, GIN, MINT}, true},
+		{{VODKA, WHISKEY, TEQUILA, COLA, RUM, GIN}, true}
+	});
+
+	Client* group1 = create_group({
+		{{VODKA}, false},
+		{{VODKA}, false},
+		{{BEER, VODKA}, false},
+		{{BEER, VODKA}, false}
+	});
+
+	Client* group2 = create_group({
+		{{COLA}, false},
+		{{GIN, COLA}, false}
+		});
+
+	vector<Client*> groups = { group1, group2 };
+
+	std::cout << placeAll(bar, groups);
+
+	clear(bar);
+	clear(group1);
+	clear(group2);
+
+	return 0;
+}
---
+++ 11-exam-prep/Task2.cpp
@@ -0,0 +1,300 @@
+﻿#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using std::queue;
+using std::vector;
+using std::string;
+using std::stringstream;
+
+struct node {
+	int data;
+	vector<node*> children;
+
+	node(int data = 0, vector<node*> children = vector<node*>())
+		:data(data), children(children) {
+	}
+};
+
+class Tree {
+public:
+	Tree() : name(""), root(nullptr) {}
+	Tree(string name) : name(name) {
+		try {
+			read_from_file();
+		}
+		catch(std::exception& e){
+			std::cerr << e.what();
+			free(root);
+		}
+	}
+
+	Tree(const Tree& other) {
+		root = copy(other.root);
+	}
+
+	Tree& operator=(const Tree& other) {
+		if (this != &other) {
+			free(root);
+			root = copy(other.root);
+		}
+
+		return *this;
+	}
+
+	~Tree() {
+		free(root);
+	}
+
+	bool has_subtree(const Tree& other) {
+		return has_subtree(root, other.root);
+	}
+
+	void remove_subtree(const Tree& other) {
+		while (node* subtree = find_subtree(root, other.root)) {
+			remove_subtree(subtree, other.root);
+		}
+	}
+
+	void print_by_levels() const {
+		if (!root) return;
+
+		std::cout << "| " << root->data << " |\n";
+
+		queue<node*> level;
+		level.emplace(root);
+		level.emplace(nullptr);
+
+		while (!level.empty()) {
+			node* current = level.front();
+			level.pop();
+
+			if (!current) {
+				if (!level.empty()) {
+					std::cout << "|";
+					std::cout << std::endl;
+					level.emplace(nullptr);
+				}
+			}
+			else {
+				std::cout << "| ";
+				for (auto child : current->children) {
+					std::cout << child->data << " ";
+					level.emplace(child);
+				}
+			}
+		}
+	}
+
+private:
+	// Разделя низа на парчета според delimeter
+	static vector<string> split(const string& str, char delimiter) {
+		vector<string> tokens;
+		stringstream ss(str);
+		string token;
+
+		while (getline(ss, token, delimiter))
+			tokens.push_back(token);
+
+		return tokens;
+	}
+
+	// Маха спейсовете от началото и края на низа
+	static string trim(const string& str, char delimiter) {
+		size_t start = str.find_first_not_of(delimiter);
+		if (start == std::string::npos) {
+			return str;
+		}
+
+		size_t end = str.find_last_not_of(delimiter);
+		return str.substr(start, end - start + 1);
+	}
+
+	// Парсва групите деца от реда
+	static vector<vector<node*>> parse_row(const string& row) {
+		vector<vector<node*>> result;
+		vector<string> segments = split(trim(row, '|'), '|');
+
+		for (const string& segment : segments) {
+			vector<node*> numbers;
+			stringstream ss(trim(segment, ' '));
+			int num;
+
+			while (ss >> num)
+				numbers.push_back(new node(num));
+
+			result.push_back(numbers);
+		}
+		return result;
+	}
+
+	void read_from_file() {
+		std::ifstream in(name);
+		if (!in) return;
+
+		string buff;
+		queue<node*> parent_queue;
+		
+		// Първо четем корена
+		getline(in, buff);
+		vector<vector<node*>> root_level = parse_row(buff);
+
+		if (root_level.empty() || root_level[0].empty())
+			throw std::runtime_error("Invalid tree format: Missing root");
+		
+		root = root_level[0][0];
+		parent_queue.push(root);
+
+		// Четем останалите нива
+		while (getline(in, buff)) {
+			vector<vector<node*>> current_level = parse_row(buff);
+
+			for (const auto& sibling_group : current_level) {
+				if (parent_queue.empty())
+					throw std::runtime_error("Invalid tree format: Not enough parents");
+
+				node* parent = parent_queue.front();
+				parent_queue.pop();
+
+				parent->children = sibling_group;
+
+				// Добавяме и децата за обработване
+				for (node* child : sibling_group)
+					parent_queue.push(child);
+			}
+		}
+
+		in.close();
+	}
+
+	node* copy(const node* root) {
+		if (!root) 
+			return nullptr;
+
+		vector<node*> children;
+		for (auto child : root->children)
+			children.push_back(copy(child));
+	
+		return new node(root->data, children);
+	}
+
+	void free(node*& root) {
+		if (!root)
+			return;
+
+		for (auto child : root->children)
+			free(child);
+
+		delete root;
+		root = nullptr;
+	}
+
+	node* find_subtree(node* root, node* other) {
+		if (!root || !other)
+			return nullptr;
+
+		node* result = nullptr;
+		if (root->data != other->data)
+			for (auto child : root->children) {
+				result = find_subtree(child, other);
+				if (result)
+					return result;
+			}
+
+		else {
+			for (auto other_child : other->children) {
+				auto match = find_if(root->children.begin(), root->children.end(), [other_child](const node* child) {
+					return child->data == other_child->data; });
+
+				while (match != root->children.end() && !has_subtree(*match, other_child)) {
+					match = find_if(match, root->children.end(), [other_child](const node* child) {
+						return child->data == other_child->data; });
+				}
+
+				// Ако match е стигнало края, значи или няма дете с такава стойност,
+				// или за всички деца с такава стойност has_subtree е врърнало грешно 
+				if (match == root->children.end())
+					return nullptr;
+			}
+
+			// Ако сме стигнали дотук, всички деца, които е трябвало, са се оказали поддървета
+			return root;
+		}
+	}
+
+	bool has_subtree(node* root, node* other) {
+		//if (!root)
+		//	return false;
+
+		//if (!other)
+		//	return true;
+
+		//bool result = false;
+
+		//if (root->data != other->data)
+		//	for (auto child : root->children)
+		//		result = result || has_subtree(child, other);
+		//// Попданали сме на потеницално място, където може да е поддърво
+		//else {
+		//	for (auto other_child : other->children) {
+		//		auto match = find_if(root->children.begin(), root->children.end(), [other_child](const node* child) {
+		//			return child->data == other_child->data; });
+
+		//		while (match != root->children.end() && !has_subtree(*match, other_child)) {
+		//			match = find_if(match, root->children.end(), [other_child](const node* child) {
+		//				return child->data == other_child->data; });
+		//		}
+
+		//		// Ако match е стигнало края, значи или няма дете с такава стойност,
+		//		// или за всички деца с такава стойност has_subtree е врърнало грешно 
+		//		if (match == root->children.end())
+		//			return false;
+		//	}
+		//}
+
+		//// Ако сме стигнали дотук, всички деца, които е трябвало, са се оказали поддървета
+		//return true;
+
+		return find_subtree(root, other);
+	}
+
+	int tree_sum(node* root) const {
+		if (!root)
+			return 0;
+
+		int sum = 0;
+		for (auto child : root->children)
+			sum += tree_sum(child);
+
+		return root->data + sum;
+	}
+
+	int remove_subtree(node* root, node* other) {
+		if (!root)
+			return 0;
+
+
+	}
+
+
+	string name;
+	node* root;
+};
+
+int main() {
+	
+	Tree t("input.txt");
+	t.print_by_levels();
+	std::cout << std::endl;
+
+	Tree t2("input2.txt");
+	t2.print_by_levels();
+	std::cout << std::endl;
+
+	std::cout << t.has_subtree(t2);
+
+	return 0;
+}
